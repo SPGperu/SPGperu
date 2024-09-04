f53100bce24d83535838e917122fb606746304ad
SPGperu <cesar45632123451234@gmail.com>
2024-09-04 11:27:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-document.querySelector('.image-gallery').addEventListener('touchstart', function (e) {
-    this.classList.add('scrolling');
-}, { passive: true });
-
-document.querySelector('.image-gallery').addEventListener('touchend', function (e) {
-    this.classList.remove('scrolling');
-}, { passive: true });
-// En tu archivo script.js
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
+    const menuIcon = document.querySelector('.menu-icon');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const overlay = document.querySelector('.overlay');
+    const body = document.body;
 
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
+    function toggleMenu() {
+        mobileMenu.classList.toggle('active');
+        overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
+        body.style.overflow = body.style.overflow === 'hidden' ? '' : 'hidden';
+    }
+
+    menuIcon.addEventListener('click', toggleMenu);
+    overlay.addEventListener('click', toggleMenu);
+
+    // Cerrar el menú al hacer clic en un enlace
+    const mobileMenuLinks = document.querySelectorAll('.mobile-menu-links a');
+    mobileMenuLinks.forEach(link => {
+        link.addEventListener('click', toggleMenu);
     });
 
-    // Cierra el menú si se hace clic fuera de él
-    document.addEventListener('click', function(event) {
-        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-            menu.classList.remove('active');
+    // Función para manejar el cambio de tamaño de la ventana
+    function handleResize() {
+        if (window.innerWidth >= 768) {
+            mobileMenu.classList.remove('active');
+            overlay.style.display = 'none';
+            body.style.overflow = '';
         }
-    });
+    }
+
+    // Escuchar el evento de cambio de tamaño de la ventana
+    window.addEventListener('resize', handleResize);
+
+    // Llamar a la función una vez al cargar para establecer el estado inicial
+    handleResize();
 });
-
-
